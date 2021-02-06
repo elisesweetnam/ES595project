933b3bb11ef0504e2ece713ebdd51712ff3a422f
elisesweetnam <76973786+elisesweetnam@users.noreply.github.com>
2021-02-06 16:18:08 +0000

    Add files via upload

--- es595email.py
+++ es595email.py
@@ -1,16 +1,45 @@
+# sending an email with hyperlink
 import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-port = 465  # For SSL
-smtp_server = "smtp.gmail.com"
-sender_email = "es595project@gmail.com"  # Senders email address
-receiver_email = "es595recipient@gmail.com"  # receivers email address
-password = input("Type your password and press enter: ") #input() used so i don't need to save my password within the code
-message = """\
-Subject: Hi there
+sender_email = "es595project@gmail.com"
+receiver_email = "es595recipient@gmail.com"
+password = input("Type your password and press enter:")
 
-This is a test email for ES595 project."""
+message = MIMEMultipart("alternative")
+message["Subject"] = "ALERT"
+message["From"] = sender_email
+message["To"] = receiver_email
 
+# Create the plain-text and HTML version of your message
+text = """\
+Hi,
+The patient has triggered an alert:
+http://192.168.0.40/movement"""
+html = """\
+<html>
+  <body>
+    <p>Hi,<br>
+       <a href="http://192.168.0.40/movement">The patient</a> 
+       has triggered an alert.
+    </p>
+  </body>
+</html>
+"""
+
+# Turn these into plain/html MIMEText objects
+part1 = MIMEText(text, "plain")
+part2 = MIMEText(html, "html")
+
+# The email client will try to render the last part first
+message.attach(part1)
+message.attach(part2)
+
+# Create secure connection with server and send email
 context = ssl.create_default_context()
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
     server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+    server.sendmail(
+        sender_email, receiver_email, message.as_string()
+    )
