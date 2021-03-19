c487003bc9ac793df0e46841188ad897da4300aa
elisesweetnam <76973786+elisesweetnam@users.noreply.github.com>
2021-03-19 17:36:11 +0000

    Delete demo.py

--- demo.py
+++
@@ -1,9 +0,0 @@
-def demoUtil():
-    txt = 'Dobbin'
-    msg = 'Weather'
-    num = "1.23456"
-    print("Hello {} nice {}".format(txt, msg))
-    print(int(float(num)))
-
-if __name__ == "__main__":
-    demoUtil()
