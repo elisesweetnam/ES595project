4783a20233877102e661181161fb5297e5c7d506
elisesweetnam <elisesweetnam@gmail.com>
2021-03-20 18:29:06 +0000

    d

--- architecture/data_util.py
+++ architecture/data_util.py
@@ -10,14 +10,10 @@ def handleData():
     This module handles data received from the sensor via http
     '''
     # reading data from arduino board 
-    print("handling data")
-    # may need to pip install requests
     
-
     # make requests every second, forever
     readings_l = []  # start with an empty data structure
     starttime = time.time()
-    print('To stop this program, KILL THIS TERMINAL')
     while True:
         time.sleep(1.0 - ((time.time() - starttime) % 1.0)) # locks to system clock, sleeps every 1.0 seconds
         url = 'http://192.168.0.20/movement' # '192.168.0.40/movement'
@@ -25,7 +21,7 @@ def handleData():
         pin_list=resp.text.split('-')
         # print(resp.text)
         email_util.handleEmail(pin_list) 
-        db.write_db.handleWriting(pin_list)
+        db.write_db.handleWriting(pin_list) #sends pin_list to write_db
 
     #Change resp to movement 
     # ThisReading = resp
